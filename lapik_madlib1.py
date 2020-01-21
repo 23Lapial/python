a253e7651370943f4f0c3fbcc1950a35cc3f2b96
23Lapial <59451344+23Lapial@users.noreply.github.com>
2020-01-21 10:43:24 -0500

    Add files via upload

---
+++ lapik_madlib1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = input('enter a name: ')
+noun = input('enter a noun: ')
+adjective = input('enter an adjective: ')
+
+print(f'\n{name} has a long list of sights to see, including the\n{noun} museum and the {adjective} park.')
