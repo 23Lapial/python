af36fd7a67edc6482ff8ac6168b92fbf0f2eec60
23Lapial <59451344+23Lapial@users.noreply.github.com>
2020-01-22 08:00:06 -0500

    Add files via upload

---
+++ lapik_madlib2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# declaring variables and prompting user with input
+name = input('\nenter a name: ')
+noun = input('enter a noun: ')
+adj = input('enter an adjective: ')
+verb = input('enter a verb: ')
+adverb = input('enter an adverb: ')
+
+
+# using formating strings to help produce an output
+print(f'\n{name} likes to {verb} with a {adj} {noun} at the park, but people say {name} likes to {verb} REALLY {adverb} so this is very important to know.')
