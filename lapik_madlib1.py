5f245dc81494138f258059ba317c744b38a59230
23Lapial <59451344+23Lapial@users.noreply.github.com>
2020-01-24 12:44:38 -0500

    Delete lapik_madlib1.py

--- lapik_madlib1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-name = input('enter a name: ')
-noun = input('enter a noun: ')
-adjective = input('enter an adjective: ')
-
-print(f'\n{name} has a long list of sights to see, including the\n{noun} museum and the {adjective} park.')
