5f02b0ef8ba5b5b8db5390b143413a8589281a27
23Lapial <59451344+23Lapial@users.noreply.github.com>
2020-01-24 12:33:06 -0500

    Update lapik_madlib2.py

--- lapik_madlib2.py
+++ lapik_madlib2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# using a dictionary to label the user's input in a variable named madlib.
+# using a dictionary to store the user's input and label in a variable named madlib.
 madlib = {'name':input('enter a name: '),'noun':input('enter a noun: '),'adj':input('enter an adjective: '),'verb':input('enter a verb: '),'adverb':input('enter an adverb: ')}
 
 # output with the madlib values placed in appropriate order.
